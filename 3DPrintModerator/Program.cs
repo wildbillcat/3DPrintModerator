7e8dd2ea9c12aac762c4d45181fb281b5da6fef3
Patrick McMorran <patrick.mcmorran@yale.edu>
2014-09-17 23:25:42 -0400

    First Commit, trying to figure out how to find 3D printers using the windows API

---
+++ 3DPrintModerator/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Printing;
+using System.Runtime.InteropServices;
+using Windows.Graphics.Printing;
+
+namespace _3DPrintModerator
+{
+    [FlagsAttribute]
+    enum PrinterEnumFlags
+    {
+        PRINTER_ENUM_DEFAULT = 0x00000001,
+        PRINTER_ENUM_LOCAL = 0x00000002,
+        PRINTER_ENUM_CONNECTIONS = 0x00000004,
+        PRINTER_ENUM_FAVORITE = 0x00000004,
+        PRINTER_ENUM_NAME = 0x00000008,
+        PRINTER_ENUM_REMOTE = 0x00000010,
+        PRINTER_ENUM_SHARED = 0x00000020,
+        PRINTER_ENUM_NETWORK = 0x00000040,
+        PRINTER_ENUM_EXPAND = 0x00004000,
+        PRINTER_ENUM_CONTAINER = 0x00008000,
+        PRINTER_ENUM_ICONMASK = 0x00ff0000,
+        PRINTER_ENUM_ICON1 = 0x00010000,
+        PRINTER_ENUM_ICON2 = 0x00020000,
+        PRINTER_ENUM_ICON3 = 0x00040000,
+        PRINTER_ENUM_ICON4 = 0x00080000,
+        PRINTER_ENUM_ICON5 = 0x00100000,
+        PRINTER_ENUM_ICON6 = 0x00200000,
+        PRINTER_ENUM_ICON7 = 0x00400000,
+        PRINTER_ENUM_ICON8 = 0x00800000,
+        PRINTER_ENUM_HIDE = 0x01000000
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct PRINTER_INFO_2
+    {
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pServerName;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pPrinterName;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pShareName;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pPortName;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pDriverName;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pComment;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pLocation;
+        public IntPtr pDevMode;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pSepFile;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pPrintProcessor;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pDatatype;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string pParameters;
+        public IntPtr pSecurityDescriptor;
+        public uint Attributes; // See note below!
+        public uint Priority;
+        public uint DefaultPriority;
+        public uint StartTime;
+        public uint UntilTime;
+        public uint Status;
+        public uint cJobs;
+        public uint AveragePPM;
+    }
+
+    class Program
+    {
+        [DllImport("User32.dll")]
+        public static extern int MessageBox(int h, string m, string c, int type);
+
+        [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool EnumPrinters(PrinterEnumFlags Flags, string Name, uint Level, IntPtr pPrinterEnum, uint cbBuf, ref uint pcbNeeded, ref uint pcReturned);
+
+        private const int ERROR_INSUFFICIENT_BUFFER = 122;
+
+        public static PRINTER_INFO_2[] enumPrinters(PrinterEnumFlags Flags)
+        {
+            uint cbNeeded = 0;
+            uint cReturned = 0;
+            if (EnumPrinters(Flags, null, 2, IntPtr.Zero, 0, ref cbNeeded, ref cReturned))
+            {
+                return null;
+            }
+            int lastWin32Error = Marshal.GetLastWin32Error();
+            if (lastWin32Error == ERROR_INSUFFICIENT_BUFFER)
+            {
+                IntPtr pAddr = Marshal.AllocHGlobal((int)cbNeeded);
+                if (EnumPrinters(Flags, null, 2, pAddr, cbNeeded, ref cbNeeded, ref cReturned))
+                {
+                    PRINTER_INFO_2[] printerInfo2 = new PRINTER_INFO_2[cReturned];
+                    int offset = pAddr.ToInt32();
+                    Type type = typeof(PRINTER_INFO_2);
+                    int increment = Marshal.SizeOf(type);
+                    for (int i = 0; i < cReturned; i++)
+                    {
+                        printerInfo2[i] = (PRINTER_INFO_2)Marshal.PtrToStructure(new IntPtr(offset), type);
+                        offset += increment;
+                    }
+                    Marshal.FreeHGlobal(pAddr);
+                    return printerInfo2;
+                }
+                lastWin32Error = Marshal.GetLastWin32Error();
+            }
+            throw new Exception();
+        }
+
+        static void Main(string[] args)
+        {
+            // Create a local print server
+            /*
+            LocalPrintServer ps = new LocalPrintServer();
+            
+            PrintQueueCollection myPrintQueues = ps.GetPrintQueues();
+            String printQueueNames = "My Print Queues:\n\n";
+            foreach (PrintQueue pq in myPrintQueues)
+            {
+                printQueueNames += "\t" + pq.Name + "\n";
+            }
+            Console.WriteLine(printQueueNames);
+
+            MessageBox(0, "API Message Box", "API Demo", 0);
+             */
+
+            PRINTER_INFO_2[] Orinters = enumPrinters(PrinterEnumFlags.PRINTER_ENUM_NAME);
+            foreach (PRINTER_INFO_2 Printer in Orinters)
+            {
+                Console.WriteLine(Printer.pPrinterName);
+            }
+
+            //var printManager = Windows.Graphics.Printing.PrintManager.GetForCurrentView();
+
+            Console.ReadLine();
+        }
+
+    }
+}
